851421de43821fbddd014c4868abd9b977e9cdfc
sankarshan <sankarshandesai3107@gmail.com>
2024-05-03 21:37:37 +0530

    init toolbox

---
+++ sketchbook/src/components/toolbox/index.js
@@ -0,0 +1,26 @@
+const Toolbox = () => {
+    return ( <div>
+
+        <div>
+            <h4>Stroke Color</h4>
+            <div>
+                <div/>
+            </div>
+        </div>
+        <div>
+            <h4>BRush Size</h4>
+            <div>
+                <div/>
+            </div>
+        </div>
+
+
+
+        </div>)
+
+
+
+    
+}
+
+export default Toolbox
